ab78edf7a995202700af32dde079b5486cd7376a
jarvis <jeetwork941@gmail.com>
2024-11-12 04:37:21 +0530

    user model created

---
+++ api/schemas/userModel.py
@@ -0,0 +1,17 @@
+from api import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    passwordHash = db.Column(db.String(200), nullable=False)
+
+    def setPassword(self, password):
+        self.passwordHash = generate_password_hash(password)
+
+    def checkPassword(self, password):
+        return check_password_hash(self.passwordHash, password)
